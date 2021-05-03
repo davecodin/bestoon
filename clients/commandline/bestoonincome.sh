50f5918ece13382be8a0aee8db31119e5536fa96
hosein <hosein.bonham@gmail.com>
2021-05-03 11:55:19 +0430

    better scripts and removing some csrf checking

--- clients/commandline/bestoonincome.sh
+++ clients/commandline/bestoonincome.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+source "$(dirname $0)"/bestoonconfig.sh
 
-source config.sh
+print_usage()
+{
+    echo "Use this script to submit expense reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
 
-curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/income/
+AMOUNT=$1
+shift
+TEXT=$*
+if [ -z "$TEXT" ]; then
+    print_usage
+    exit 1
+fi
 
-
-
-
+curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT" $BASE_URL/submit/expense/
 
