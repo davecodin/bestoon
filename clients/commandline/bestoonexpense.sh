4f9c47d3b060a37433229244d1f5c5a74c788efc
hosein <hosein.bonham@gmail.com>
2021-05-03 12:09:53 +0430

    edit client scripts

--- clients/commandline/bestoonexpense.sh
+++ clients/commandline/bestoonexpense.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+source "$(dirname $0)"/bestoonconfig.sh
 
+print_usage()
+{
+    echo "Use this script to submit expense reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
 
-source config.sh
+AMOUNT=$1
+shift
+TEXT=$*
+if [ -z "$TEXT" ]; then
+    print_usage
+    exit 1
+fi
 
-
-curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/expense/
-
+curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT" $BASE_URL/submit/expense/
 
